dc5e14324f124bab04ab683465c084b3fb6e29ad
nimajji <nimajji48@gmail.com>
2025-02-13 21:15:36 +0530

    Update music.js

--- music.js
+++ music.js
@@ -4,13 +4,9 @@ const audio = document.getElementById('valentine-music');
 // Get the card element
 const card = document.querySelector('.card');
 
-// Play music when the card is clicked
-card.addEventListener('click', () => {
-  if (audio.paused) {
-    audio.play();
-  } else {
-    audio.pause();
-    audio.currentTime = 0; // Reset audio to the beginning
-  }
+// Allow user to start music manually
+playButton.addEventListener('click', () => {
+  audio.play();
+  playButton.style.display = 'none'; // Hide the button after clicking
 });
 audio.volume = 0.5; // Set volume to 50%
